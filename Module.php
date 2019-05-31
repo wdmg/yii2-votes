f39d0d798c09ce097ad100e70d411623062b5946
alex-wdmg <wdmg.com.ua@gmail.com>
2019-06-01 02:47:12 +0300

    Update Yii2 version

--- Module.php
+++ Module.php
@@ -6,7 +6,7 @@ namespace wdmg\votes;
  * Yii2 Votes
  *
  * @category        Module
- * @version         0.0.2
+ * @version         0.0.3
  * @author          Alexsander Vyshnyvetskyy <alex.vyshnyvetskyy@gmail.com>
  * @link            https://github.com/wdmg/yii2-votes
  * @copyright       Copyright (c) 2019 W.D.M.Group, Ukraine
@@ -54,7 +54,7 @@ class Module extends \yii\base\Module
     /**
      * @var string the module version
      */
-    private $version = "0.0.2";
+    private $version = "0.0.3";
 
     /**
      * @var integer, priority of initialization
